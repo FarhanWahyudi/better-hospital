7e6dbbdec1aaa3b68eeac817ed3b328d40ef4c67
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-06-29 14:09:06 +0700

    Membuat hospital specialist controller

--- app/Http/Controllers/HospitalSpecialistController.php
+++ app/Http/Controllers/HospitalSpecialistController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\HospitalSpecialistRequest;
+use App\Services\HospitalService;
+use App\Services\HospitalSpecialistService;
 use Illuminate\Http\Request;
 
 class HospitalSpecialistController extends Controller
 {
-    //
+    private HospitalService $hospitalService;
+
+    public function __construct(HospitalService $hospitalService) {
+        $this->hospitalService = $hospitalService;
+    }
+
+    public function attach(HospitalSpecialistRequest $request, int $hospitalId)
+    {
+        $validated = $request->validated();
+        
+        $this->hospitalService->attachSpecialist($hospitalId, $validated['specialist_id']);
+        return response()->json([
+            'message' => 'Specialist attached successfully'
+        ]);
+    }
+
+    public function detach(int $hospitalId, int $specialistId)
+    {
+        $this->hospitalService->detachSpecialist($hospitalId, $specialistId);
+        return response()->json([
+            'message' => 'Specialist detached successfully'
+        ]);
+    }
 }
